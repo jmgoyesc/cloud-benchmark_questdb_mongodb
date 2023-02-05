7121ed6f6ae2cb55e041d0602d0d49111375e535
Juan Manuel Goyes Coral <jmgoyesc@gmail.com>
2023-02-05 01:18:40 +0100

    agent. add app version to the log at start up

--- agent/src/main/java/com/github/jmgoyesc/agent/domain/services/VersionService.java
+++ agent/src/main/java/com/github/jmgoyesc/agent/domain/services/VersionService.java
@@ -1,7 +1,9 @@
 package com.github.jmgoyesc.agent.domain.services;
 
 import com.github.jmgoyesc.agent.domain.models.VersionInfo;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +13,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class VersionService {
 
     private final BuildProperties properties;
+
+    @PostConstruct
+    public void init() {
+        log.info("Agent app started. version: {} - build time: {}", properties.getVersion(), properties.getTime());
+    }
 
     public VersionInfo get() {
         return VersionInfo.build(properties);
