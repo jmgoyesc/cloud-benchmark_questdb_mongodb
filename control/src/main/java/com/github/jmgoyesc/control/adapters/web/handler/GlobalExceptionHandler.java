8d8e4b80a4be3c227c2bd550b6ad0b6b48921e4e
Juan Manuel Goyes Coral <jmgoyesc@gmail.com>
2023-02-06 20:59:19 +0100

    control. add common exception handler

--- control/src/main/java/com/github/jmgoyesc/control/adapters/web/handler/GlobalExceptionHandler.java
+++ control/src/main/java/com/github/jmgoyesc/control/adapters/web/handler/GlobalExceptionHandler.java
@@ -1,22 +1,13 @@
 package com.github.jmgoyesc.control.adapters.web.handler;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
  * @author Juan Manuel Goyes Coral
  */
 
-//TODO: add handler exception for invlaid enum in database
-
 @ControllerAdvice
-public class GlobalExceptionHandler {
-
-    @ExceptionHandler
-    public ProblemDetail onException(IllegalArgumentException e) {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
-    }
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 }
